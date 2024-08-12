88d062dc978401d26e63ed78894be6b02c92a7cd
Jake Rasmusson <jakerasmusson@gmail.com>
2024-08-12 18:16:14 -0500

    add worker.js to public folder

---
+++ client/public/worker.js
@@ -0,0 +1,29 @@
+console.log('Worker started!')
+
+
+    const handleEvalAnswer = data => {
+      const testResults = []
+      const userOutput = []
+      const { codeToRun, answers, codeTests } = data
+      
+      for (i = 0; i < codeTests.length; i++) {
+        const result = eval(`${codeToRun}  
+          ${codeTests[i]}`)
+        testResults.push(result === answers[i])
+        userOutput.push(result)
+      }
+      
+      // const messageType = result === data.answer
+      //   ? 'CORRECT'
+      //   : 'INCORRECT'
+    
+      postMessage({ testResults, userOutput })
+    }
+    
+    
+    onmessage = e => {
+      switch(e.data.type) {
+        case 'EVAL_ANSWER':
+          handleEvalAnswer(e.data)
+      }
+    }
