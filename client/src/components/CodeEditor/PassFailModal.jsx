32b92a3d8d3a718ad65c29cfb7d210e8657f84e5
ktunebe <ktunebe@gmail.com>
2024-08-15 20:35:58 -0500

    modal changes

--- client/src/components/CodeEditor/PassFailModal.jsx
+++ client/src/components/CodeEditor/PassFailModal.jsx
@@ -12,6 +12,7 @@ const PassFailModal = ({
 	setIsOpen,
 	data,
 	testResultsArray,
+	userOutputArray,
 	answerStatus,
 }) => {
 	return (
@@ -22,31 +23,35 @@ const PassFailModal = ({
 			<DialogBackdrop className="fixed inset-0 bg-black/80" />
 			<div className="fixed inset-0 w-screen overflow-y-auto p-4">
 				<div className="flex min-h-full items-center justify-center">
-					<DialogPanel className="w-1/2 space-y-4 border bg-jet text-lightGray p-12">
+					<DialogPanel className="w-3/4 space-y-4 border bg-jet text-lightGray p-12">
 						<DialogTitle className="font-bold">{`${
-							answerStatus ? 'You passed!' : 'You failed :('
+							answerStatus ? 'You passed!' : 'You failed!'
 						}`}</DialogTitle>
 						<Description>Test Results: </Description>
 						{data.problem.tests.map((test, index) => {
 							const answer = data.problem.answers[index]
+							const userOutput = userOutputArray[index]
 							const result = testResultsArray[index]
 							console.log(testResultsArray)
 							return (
-								<p
+								<div
 									key={test}
-									className={`text-white ${
+									className={`text-white border-4 p-4 rounded`}>
+										<p>{`Expect ${test} to equal: ${answer}.`}</p> <p className={`rounded w-1/2 ${
 										result ? 'bg-green-800' : 'bg-lannisterRed'
-									}`}>{`Expect ${test} to equal ${answer}.`}</p>
+									}`}>Your output: {userOutput}</p>
+								</div>
+								
 							)
 						})}
 					<div className="flex flex-col">
 						<div className="nes-balloon from-right self-end">
-							<p className="self-start">Josh's Message Here!!</p>
+							<p className="self-start">You smell!!</p>
 						</div>
 						<img className="self-end" src="misc-images/archmage.png" />
 					</div>
 						<Button
-							className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700"
+							className={`rounded ${answerStatus ? 'bg-green-800' : 'bg-lannisterRed'} py-2 px-4 text-sm border-2 text-white data-[hover]:bg-jet data-[active]:bg-jet`}
 							onClick={() => setIsOpen(false)}>
 							{`${answerStatus ? 'Continue' : 'Try Again'}`}
 						</Button>
